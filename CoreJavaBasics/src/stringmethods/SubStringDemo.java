13b2590597f6b07af9aa4d5f3d80366939895b62
garciaedy <edygarcia97@gmail.com>
2023-09-26 22:22:16 -0400

    modify arrays class

--- CoreJavaBasics/src/stringmethods/SubStringDemo.java
+++ CoreJavaBasics/src/stringmethods/SubStringDemo.java
@@ -3,7 +3,24 @@ package stringmethods;
 public class SubStringDemo {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+
+		//--------------0123456789012345
+		String string = "This is a string";
+
+		// substring with a start and an end
+		String sub = string.substring (5, 7);
+		System.out.println(sub);
+		
+		
+		// substring with just a start
+		String sub1 = string.substring(5);
+		System.out.println(sub1);
+
+		//==============-0123456789012
+		String filename = "someimage.jpg";
+		int lastDot = filename.lastIndexOf (".");
+
+		String ext = filename.substring (filename.lastIndexOf (" .") + 1);
 
 	}
 
