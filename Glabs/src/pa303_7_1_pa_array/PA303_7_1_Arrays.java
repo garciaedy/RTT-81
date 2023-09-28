9ce2796a429ab800f9168f192f6c405fee8f04d7
garciaedy <edygarcia97@gmail.com>
2023-09-27 21:42:09 -0400

    glab 11

--- Glabs/src/pa303_7_1_pa_array/PA303_7_1_Arrays.java
+++ Glabs/src/pa303_7_1_pa_array/PA303_7_1_Arrays.java
@@ -1,6 +1,8 @@
 package pa303_7_1_pa_array;
 
 import java.util.Arrays;
+import java.util.Scanner;
+
 
 public class PA303_7_1_Arrays {
 
@@ -138,7 +140,25 @@ public class PA303_7_1_Arrays {
 
 		
 
-	
+	     // Task 11: Write some Java code that asks the user how many favorite things
+		// they have. Based on their answer, you should create a String array of the
+		// correct size. Then ask the user to enter the things and store them in the
+		// array you created. Finally, print out the contents of the array.
+
+	        Scanner scanner = new Scanner(System.in);
+
+	        System.out.println("How many favorite things? ");
+	        int numFavorites = scanner.nextInt();
+	        scanner.nextLine();
+
+	        String[] favorites = new String[numFavorites];
+
+	        for ( int pos = 0 ; pos < numFavorites ; pos++ ) {
+	        	System.out.println("Enter favrotie thing #" + pos);
+	        	favorites[pos] = scanner.nextLine();
+	        }
+
+	        System.out.println(Arrays.toString(favorites));
 
 	}
 
