503664c8bf3e49dc1338e2bc7a8959258cb930ab
garciaedy <edygarcia97@gmail.com>
2023-09-27 22:00:49 -0400

    calculator

---
+++ CoreJavaBasics/src/calc/Calculator.java
@@ -0,0 +1,22 @@
+package calc;
+
+public class Calculator {
+
+	public static void main(String[] args) {
+	/* Create a small calculator .. it will be able to do 4 operations - add, subtract, multiply and divide.
+	 *   Each of these will be a single method that does that operation.    Ask the user for 2 numbers 
+	 *   and then ask the user which operation they want to do.   Then do that operation and print the result.   
+	 *   Each operation should not print its own results.   All operations should work with double values. (edited) 
+	So this should be a main function with probably 6 methods ( 4 operations, 1 menu, 1 input )
+	each operation should take 2 parameters and return a double ...*/
+		Calculator c = new Calculator();
+		c.menu();
+	}
+	
+		public void menu() {
+		// start writing your code here
+		
+		
+	}
+
+}
