58166e43c725f47fe3bda567449ca8c203c34297
garciaedy <edygarcia97@gmail.com>
2023-09-27 22:04:52 -0400

    updated coffee shop

--- CoreJavaBasics/src/coffeeshop/Product.java
+++ CoreJavaBasics/src/coffeeshop/Product.java
@@ -2,25 +2,33 @@ package coffeeshop;
 
 public class Product {
 	
-
 	private String name;
 	private double price;
 	
-	public String getName () {
-	return name;
+	public Product() {
+		
 	}
 	
-	public void setName (String name) {
-	this.name = name;
+	public Product(String name, double price) {
+		this.name = name;
+		this.price = price;
 	}
 	
-	public double getPrice () {
-	return price;
-	}
-	public void setPrice (double price) {
-	this.price = price;
 
+	public String getName() {
+		return name;
 	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public double getPrice() {
+		return price;
+	}
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
 }
-
-
