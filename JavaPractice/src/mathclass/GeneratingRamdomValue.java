4f6f228d34ab20efb27ccca4951f7cfb9e1a3dc5
garciaedy <edygarcia97@gmail.com>
2023-09-29 21:46:19 -0400

    Random classes

--- JavaPractice/src/mathclass/GeneratingRamdomValue.java
+++ JavaPractice/src/mathclass/GeneratingRamdomValue.java
@@ -4,9 +4,20 @@ import java.util.Random;
 public class GeneratingRamdomValue {
 
 	public static void main(String[] args) {
-		
+
 		
 		Random random = new Random();
+		
+		int x =random.nextInt(100) +1;
+		double y = random.nextDouble();
+		boolean z = random.nextBoolean();
+		
+		System.out.println(x);
+		System.out.println(y);
+		System.out.println(z);
+
+		
+		
 		
 	}
 
