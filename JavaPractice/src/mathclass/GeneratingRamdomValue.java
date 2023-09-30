31e820fad3da73af58438f201934aa47ecb90892
garciaedy <edygarcia97@gmail.com>
2023-09-29 21:22:58 -0400

    Random class + imported Random library

---
+++ JavaPractice/src/mathclass/GeneratingRamdomValue.java
@@ -0,0 +1,13 @@
+package mathclass;
+import java.util.Random;
+
+public class GeneratingRamdomValue {
+
+	public static void main(String[] args) {
+		
+		
+		Random random = new Random();
+		
+	}
+
+}
