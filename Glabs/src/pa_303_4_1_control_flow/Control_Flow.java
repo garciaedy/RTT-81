b63ad14d1cc2b050f1de68fe3e72d11a51c39bd2
garciaedy <edygarcia97@gmail.com>
2023-10-01 14:08:54 -0400

    prompt 6

--- Glabs/src/pa_303_4_1_control_flow/Control_Flow.java
+++ Glabs/src/pa_303_4_1_control_flow/Control_Flow.java
@@ -107,7 +107,38 @@ public class Control_Flow {
 		 * the corresponding weekday. Print “Out of range” if the number is less than 1 or greater than 7.
 		 *  Do not forget to include “break” statements in each of your cases.*/
 		
-		
+
+				System.out.println("Enter the day of the week: ");
+				int day=input.nextInt();
+				
+				switch(day)
+				{
+					case 1:
+						    System.out.println("Monday");
+						    break;
+					case 2:
+					    System.out.println("Tuesday");
+					    break;
+					case 3:
+					    System.out.println("Wednesday");
+					    break;
+					case 4:
+					    System.out.println("Thursday");
+					    break;
+					case 5:
+					    System.out.println("Friday");
+					    break;
+					case 6:
+					    System.out.println("Saturday");
+					    break;
+					case 7:
+					    System.out.println("Sunday");
+					    break;
+					default:
+						System.out.println("Out of Range");
+						break;
+				}
+				
 		
 		/* 7. Create a program that lets the users input their filing status and income. Display how much tax the user would 
 		 * have to pay according to status and income.
