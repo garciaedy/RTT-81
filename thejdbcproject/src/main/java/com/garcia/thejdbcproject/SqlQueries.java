77269ba32c079052c2767f5e733ad7b110eeb9c0
garciaedy <edygarcia97@gmail.com>
2023-10-28 17:51:02 -0400

    importing sql to project

---
+++ thejdbcproject/src/main/java/com/garcia/thejdbcproject/SqlQueries.java
@@ -0,0 +1,6 @@
+package com.garcia.thejdbcproject;
+
+public class SqlQueries {
+	public final static String GetEmployeeById= "Select * from employees where  employeeNumber=?";
+	public final static String GetAllEmployees= "Select * from employees";
+}
