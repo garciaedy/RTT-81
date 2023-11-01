5de8807e0a5cbe3b4d64820b31997a7afd9fd909
garciaedy <edygarcia97@gmail.com>
2023-10-31 22:10:22 -0400

    created a Junit project

---
+++ glab306/src/main/java/com/garcia/glab306/App.java
@@ -0,0 +1,13 @@
+package com.garcia.glab306;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
