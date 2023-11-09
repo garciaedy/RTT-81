2a20ebe307cbb15c61b606ab576983d679389997
garciaedy <edygarcia97@gmail.com>
2023-11-08 22:15:28 -0500

    loop practice

--- CoreJava/src/forloops/ForLooops.java
+++ CoreJava/src/forloops/ForLooops.java
@@ -10,6 +10,68 @@ public class ForLooops {
 		System.out.println(" ");
 		System.out.println("_________________________________");
 		System.out.println(" ");
+		// Sum numbers from 1 to 10
+
+		int sum =0;
+		for(int i =0; i<=10; i++) {
+			sum +=i;
+		}
+		
+		System.out.println(sum);
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+		// Print a multiplication table for 10
+		for(int m=1; m<=10; m++) {
+			System.out.println("10 * " + m + " = " + (10*m));
+		}
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+
+		
+	//	Enhanced For Loops
+		String[] fruits = {"Apple", "Banana", "Cherry"};
+		for(String fruit : fruits) {
+			System.out.println(fruit );
+		}
+		
+//		for(int i =0; i<fruits.length; i++) {
+//			System.out.println(fruits[i]);
+//		}
+//		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+		
+		
+		
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
 
 	}
 }
