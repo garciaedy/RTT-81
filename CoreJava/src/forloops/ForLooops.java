8ea938a6f49044e9fe3e542a759270d6c6eee6d4
garciaedy <edygarcia97@gmail.com>
2023-11-08 21:48:20 -0500

    loop practice

---
+++ CoreJava/src/forloops/ForLooops.java
@@ -0,0 +1,15 @@
+package forloops;
+
+public class ForLooops {
+	public static void main(String[] args) {
+		
+		for(int i =0; i<=10; i++) {
+			System.out.println(i);
+		}
+		
+		System.out.println(" ");
+		System.out.println("_________________________________");
+		System.out.println(" ");
+
+	}
+}
